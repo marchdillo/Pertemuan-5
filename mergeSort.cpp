91b1802b8c37274f519d01044c72f8a8f245c878
marchdillo <kevinno.limiy@gmail.com>
2025-04-17 11:53:43 +0700

    step 4.e

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -65,4 +65,11 @@ void mergeSort(int low, int high)
         }
         k++;
     }
+
+    while (j <= high)
+    {                  // step 4.e
+        B[k] = arr[j]; // step 4.e.i
+        j++;           // step 4.e.ii
+        k++;           // step 4.e.iii
+    }
 }
